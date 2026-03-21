9cc7c77bdf7216a87c5bbf033ff541114f8e22e5
Milly-coder17 <epanganiban@gbox.adnu.edu.ph>
2026-03-21 14:24:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const categoryButtons = document.querySelectorAll(".categoriesContainer button")
 let currentSelectedType = "AllTypes";
 
 const items = [
-    {name: "4 Tumblers", location: "ADNU, Covered Court", date: "2025-12-16", type: "tumbler", image: "images/Bottles.jpeg"},
-    {name: "3 Umbrellas", location: "Plaza Rizal", date: "2025-12-19", type: "umbrella", image: "images/Umbrellas.jpeg"},
-    {name: "Type C Charger", location: "ADNU, Bonoan", date: "2026-03-07", type: "electronics", image: "images/charger.png"},
-    {name: "Blue Cap", location: "ADNU, Bonoan", date: "2026-01-05", type: "accessories", image: "images/Blue Cap.png"},
-    {name: "Laptop and keyboard", location: "ADNU, P216", date: "2026-01-05", type: "electronics", image: "images/laptop and keyboard.png"},
-    {name: "Books and papers", location: "SM Naga", date: "2026-01-06", type: "schoolitems documents", image: "images/books.png"},
-    {name: "Wallet ni Ma'am Wisa", location: "ADNU, ADMIN BLDG", date: "2025-06-26", type: "wallet", image: "images/wallet admin bldg.jpeg"},
-    {name: "Black cap", location: "Crispy King", date: "2025-07-04", type: "accessories", image: "images/black cap.jpg"},
-    {name: "White and Black Shirts", location: "ADNU, Arrupe", date: "2025-03-11", type: "clothing", image: "images/shirts.jpg"},
-    {name: "Black wallet", location: "ADNU, Main Gate", date: "2025-03-11", type: "wallet", image: "images/black wallet.jpeg"},
-    {name: "Coffee Bottle", location: "Magsaysay Starbucks", date: "2025-09-22", type: "tumbler", image: "images/missing coffee.png"},
-    {name: "Watch", location: "Ateneo Avenue 7/11", date: "2025-03-10", type: "accessories", image: "images/watch.jpg"},
+    {name: "4 Tumblers", location: "Covered Court", date: "2025-12-16", type: "tumbler", image: "images/Bottles.jpeg"},
+    {name: "3 Umbrellas", location: "Library", date: "2025-12-19", type: "umbrella", image: "images/Umbrellas.jpeg"},
+    {name: "Type C Charger", location: "Bonoan", date: "2026-03-07", type: "electronics", image: "images/charger.png"},
+    {name: "Blue Cap", location: "Bonoan", date: "2026-01-05", type: "accessories", image: "images/Blue Cap.png"},
+    {name: "Laptop and keyboard", location: "P216", date: "2026-01-05", type: "electronics", image: "images/laptop and keyboard.png"},
+    {name: "Books and papers", location: "Library", date: "2026-01-06", type: "schoolitems documents", image: "images/books.png"},
+    {name: "Wallet", location: "ADMIN BLDG", date: "2025-06-26", type: "wallet", image: "images/wallet admin bldg.jpeg"},
+    {name: "Black cap", location: "P216", date: "2025-07-04", type: "accessories", image: "images/black cap.jpg"},
+    {name: "White and Black Shirts", location: "Arrupe Convention Hall", date: "2025-03-11", type: "clothing", image: "images/shirts.jpg"},
+    {name: "Black wallet", location: "Main Gate", date: "2025-03-11", type: "wallet", image: "images/black wallet.jpeg"},
+    {name: "Coffee Bottle", location: "Starbucks", date: "2025-09-22", type: "tumbler", image: "images/missing coffee.png"},
+    {name: "Watch", location: "Covered Court", date: "2025-03-10", type: "accessories", image: "images/watch.jpg"},
 ];
 
-/* Code for rendering items */
 function renderItems(itemArray) {
     container.innerHTML = ``;
     itemArray.forEach(item => {
@@ -34,8 +33,7 @@ function renderItems(itemArray) {
                 <h4 class="location">📍${item.location}</h4>
                 <h4>📅 ${item.date}</h4>
             </div>
-        `
-        ;
+        `;
         container.appendChild(card);
     });
 }
@@ -56,7 +54,14 @@ function filterFunction(){
     renderItems(filteredItems);
 };
 
-searchBar.addEventListener("input", filterFunction);
+searchBar.addEventListener("keydown", function(event) {
+    if(event.key === "Enter"){
+        filterFunction();
+    }
+});
+
+document.getElementById("searchIcon").addEventListener("click", filterFunction);
+
 
 categoryButtons.forEach(button => {
     button.addEventListener("click", function() {
